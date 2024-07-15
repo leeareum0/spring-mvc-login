229e119626ac920458a980aaf3e8ef128db3a39d
이아름 <areum6321@gmail.com>
2024-07-15 17:08:06 +0900

    로그인 처리 - 회원가입, 회원 저장소 생성

---
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,38 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+//동시성 문제 고려 X,  실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public Optional<Member> findBtLoginId(String loginId) {
+        return findAll().stream().filter(m -> m.getLoginId().equals(loginId)).findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
