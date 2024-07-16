6fd8d0f72607b6578b7e69762ce1793fac78eb94
이아름 <areum6321@gmail.com>
2024-07-16 19:25:15 +0900

    로그인 처리 - 세션 직접 만들기, 세션 조회

--- src/main/java/hello/login/web/session/SessionManger.java
+++ src/main/java/hello/login/web/session/SessionManger.java
@@ -3,7 +3,9 @@ package hello.login.web.session;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -29,7 +31,25 @@ public class SessionManger {
     }
 
     //세션 조회
+    public Object getSession(HttpServletRequest request) {
 
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
 
     //세션 만료
+
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+
+        if (request.getCookies() == null) {
+            return null;
+        }
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny().orElse(null);
+    }
 }
