c808f43e43907c7e18c9948fc99867a6a9798204
이아름 <areum6321@gmail.com>
2024-07-19 18:01:35 +0900

    스프링 인터셉터 - 인증 체크, 인터셉터 등록

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LogInterceptor())
                 .order(1).addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor()) //인터셉터 등록
+                .order(2) //호출 순서 지정
+                .addPathPatterns("/**") //URL 패턴 지정
+                //인터셉터에서 제외할 패턴 지정
+                .excludePathPatterns("/", "/member/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
     }
 
 //    @Bean
